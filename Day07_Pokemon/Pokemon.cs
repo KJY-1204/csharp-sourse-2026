7e10107d18792c38fdf63f6dcf856b2ca1b0f526
KJY <dbvcxz@gmail.com>
2026-09-14 14:49:37 +0900

    과제 테스트

--- Day07_Pokemon/Pokemon.cs
+++ Day07_Pokemon/Pokemon.cs
@@ -6,8 +6,8 @@ public class Pokemon
     public string Name { get; set; } = string.Empty;
     public double HeightMeter => HeightDecimeter / 10.0;
     public double WeightKg => WeightHectogram / 10.0;
-    public int HeightDecimeter { get; init; }
+    public int HeightDecimeter { get; init; } //객체 초기화 블록 { ... } 허용 + 이후 수정 불가
     public int WeightHectogram { get; init; }
     public string? ImageUrl { get; init; }
-    public IReadOnlyList<string> Types { get; init; } = Array.Empty<string>();
+    public IReadOnlyList<string> Types { get; init; } = Array.Empty<string>(); //Types가 null이 되지 않도록 빈 배열로 초기화
 }
