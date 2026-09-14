7d1b2ac23a2e7535d0ca2b9474c14ed5779a920d
KJY <dbvcxz@gmail.com>
2026-09-14 14:31:44 +0900

    네트워크 통신 과제

---
+++ Day07_Pokemon/Pokemon.cs
@@ -0,0 +1,13 @@
+﻿namespace Day07_Pokemon;
+
+public class Pokemon
+{
+    public int  Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public double HeightMeter => HeightDecimeter / 10.0;
+    public double WeightKg => WeightHectogram / 10.0;
+    public int HeightDecimeter { get; init; }
+    public int WeightHectogram { get; init; }
+    public string? ImageUrl { get; init; }
+    public IReadOnlyList<string> Types { get; init; } = Array.Empty<string>();
+}
