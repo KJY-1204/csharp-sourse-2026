9a109ff5141aa5162aecc8625c0bb12947ecc2c9
KJY <dbvcxz@gmail.com>
2026-09-15 13:31:52 +0900

    과제1

---
+++ Day08_DTO_Mapper/Data/DataSources/PokemonApiDataSource.cs
@@ -0,0 +1,34 @@
+﻿using System.Net;
+using System.Net.Http.Json;
+using Day08_DTO_Mapper.Data.DTOs;
+
+namespace Day08_DTO_Mapper.Data.DataSources;
+
+public class PokemonApiDataSource : IPokemonApiDataSource
+{
+    private readonly HttpClient _httpClient;
+
+    public PokemonApiDataSource(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+        if (_httpClient.BaseAddress == null)
+        {
+            _httpClient.BaseAddress = new Uri("https://pokeapi.co/api/v2/");
+        }
+    }
+
+    public async Task<PokemonDto?> GetPokemonAsync(string pokemonName)
+    {
+        var formattedName = pokemonName.Trim().ToLowerInvariant();
+        var response = await _httpClient.GetAsync($"pokemon/{formattedName}");
+
+        if (response.StatusCode == HttpStatusCode.NotFound)
+        {
+            return null;
+        }
+
+        response.EnsureSuccessStatusCode();
+
+        return await response.Content.ReadFromJsonAsync<PokemonDto>();
+    }
+}
